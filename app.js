44e908acad1aadeb8cdc392bd49295579b2775b0
RafaCod0011 <nicolascuello12@gmail.com>
2025-05-09 11:49:07 -0300

    Test para subir a vercel
    Prueba para subir el servidor a vercel

--- app.js
+++ app.js
@@ -1,62 +1,57 @@
+const express = require("express");
+const PORT = 3000;
+const app = express();
+const fs = require("fs");
+const userController = require("./controller/Usuarios/user.js");
 
-const express= require('express');
-const PORT=3000;
-const app= express();
-const fs= require('fs');
-const userController= require('./controller/Usuarios/user.js');
-
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.set("view engine", "pug");
 app.set("views", "./views");
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/Persona", (req, res) => {
-    res.render("Persona/persona");
+  res.render("Persona/persona");
 });
 
 app.post("/persona/add", (req, res) => {
+  console.log(req.body);
+  fs.readFile("usuarios.json", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error al leer el archivo");
+    }
+    const personas = JSON.parse(data);
+    personas.push(req.body);
 
-    console.log(req.body);
-    fs.readFile('usuarios.json', (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).send("Error al leer el archivo");
-        }
-        const personas = JSON.parse(data);
-        personas.push(req.body);
-
-        fs.writeFile('usuarios.json', JSON.stringify(personas, null, 2), (err) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).send("Error al escribir el archivo");
-            }
-            res.render("Persona/listado", {personas: personas});
-        });
+    fs.writeFile("usuarios.json", JSON.stringify(personas, null, 2), (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send("Error al escribir el archivo");
+      }
+      res.render("Persona/listado", { personas: personas });
     });
-    
-})
-
+  });
+});
 
 app.get("/login", (req, res) => {
-    res.render("login/login");
+  res.render("login/login");
 });
 
 app.get("/registro", (req, res) => {
-    res.render("login/registro");
+  res.render("login/registro");
 });
 
 app.post("/registro/add", (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 
-    userController.addUser(req, res);
-})
-
+  userController.addUser(req, res);
+});
 
 // Inicio del servidor
 
 app.listen(PORT, () => {
-    console.log("Servidor corriendo en el puerto 3000");
+  console.log("Servidor corriendo en el puerto 3000");
 });
