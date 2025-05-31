9e9009e5468fbcda29fa5cbb017a47a45da4cf83
RafaCod0011 <nicolascuello12@gmail.com>
2025-05-31 20:39:27 -0300

    Test Vercel

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require("sequelize");
+const mysql2 = require("mysql2");
 
 const sequelize = new Sequelize("sis_hospital", "root", "", {
   host: "localhost",
   dialect: "mysql",
+  dialectModule: mysql2,
   logging: false,
 });
 
